9f2d8cba1fdb2ec614f314ef93e3eeee50ed76b9
adityajethwa <>
2023-03-04 18:37:02 +0530

    4_mar #1 base

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Projects() {
+  return (
+   <>
+   <div id='home'
+    // style={{backgroundImage: `url(${bg})`}}
+    className=' bg-center bg-cover bg-no-repeat h-screen flex flex-col '>
+hh
+   </div>
+   </>
+  )
+}
+
+export default Projects

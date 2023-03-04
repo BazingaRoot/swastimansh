e9f631e21ea41c808218e3d7901bb302e848b3a1
adityajethwa <>
2023-03-05 00:00:45 +0530

    Revert "Create CNAME"
    This reverts commit 57e17b77fadd3bd44074637ef9e8879f46c07307.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './assets/home'
+import './index.css'
+import './style.css'
+
+
+function App() {
+  return (
+    <>
+     <Home/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Contact() {
+  return (
+   <>
+   <div id='home'
+    // style={{backgroundImage: `url(${bg})`}}
+    className=' bg-center bg-cover bg-no-repeat h-screen flex flex-col '>
+hh
+   </div>
+   </>
+  )
+}
+
+export default Contact

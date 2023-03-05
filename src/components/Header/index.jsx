89c5d410a6f877371213a47a751fc89ddb30a2d8
adityajethwa <>
2023-03-06 00:47:19 +0530

    5_mar #1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,11 +8,12 @@ import Navbar from './Navbar'
 function Header() {
   return (
    <>
+   
    <div id='home'
     style={{backgroundImage: `url(${bg})`}}
     className=' bg-center bg-cover bg-no-repeat h-screen flex flex-col '>
     <Navbar/>
-    <div className="wrapper flex justify-between items-center h-screen w-full px-20 lg:justify-center lg:px-6">
+    {/* <div className="wrapper flex justify-between items-center h-screen w-full px-4 lg:px-20 lg:justify-center lg:px-6">
       <div className="content lg:text-center">
         <div className="btn py-3">
           <button  className=' border-2 border-[#b0aaaa] font-bold text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-4 rounded-2xl'>Welcome</button>
@@ -22,16 +23,45 @@ function Header() {
         <p className='text-white py-4 max-w-lg '>Lorem ipsum dolor tiae ullam. Saepe voluptatum ducimus adipisci pariatur. adipisicing elit.  In qui impedit ipsum dolor sit amet consectetur</p>
         <button className='  text-white text-2xl'> Connect <i className="fa-solid fa-arrow-right text-lg  p-[2px] "></i> </button>
       </div>
-      <div className="image banner-astronout lg:hidden absolute w-[300px] flex ">
-      {/* <img className='w-96 ast-img' src={logo1} alt="" /> */}
-    
-      </div>
-      <div className="image banner-astronout1 lg:hidden absolute w-[300px] flex ">
-     
-      {/* <img className='w-96 ast-img' src={logo} alt="" /> */}
-      </div>
    
+    </div> */}
+    <section class=" text-white">
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+  >
+    <div class="mx-auto max-w-3xl text-center">
+      <h1
+        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+      >
+        Understand User Flow.
+
+        <span class="sm:block"> Increase Conversion. </span>
+      </h1>
+
+      <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+        tenetur fuga ducimus numquam ea!
+      </p>
+
+      <div class="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+          href="/get-started"
+        >
+          Get Started
+        </a>
+
+        <a
+          class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+          href="/about"
+        >
+          Learn More
+        </a>
+      </div>
     </div>
+  </div>
+</section>
+
    </div>
    </>
   )
